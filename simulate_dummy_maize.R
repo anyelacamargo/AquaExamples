76c92327c38b61fdd11375412929a1a41dc242a3
Anyela Camargo Rodriguez <X992029@niab.com>
2018-12-14 16:10:52 +0000

    tutorial

--- simulate_dummy_maize.R
+++ simulate_dummy_maize.R
@@ -9,58 +9,57 @@ library(data.table)
 library(dplyr)
 
 
-source('R/Aqua_library.R')
-source('R/ReadFileLocations.R')
-source('R/ReadWeatherInputs.R')
-source('R/ReadClockParameters.R')
-source('R/ReadFieldManagement.R')
-source('R/ReadModelParameters.R')
-source('R/ReadIrrigationManagement.R')
-source('R/ReadGroundwaterTable.R')
-source('R/ComputeVariables.R')
-source('R/ComputeCropCalendar.R')
-source('R/CalculateHILinear.R')
-source('R/CalculateHIGC.R')
-source('R/ReadModelInitialConditions.R')
-source('R/PerformSimulation.R')
-source('R/ExtractWeatherData.R')
-source('R/Solution.R')
-source('R/CheckModelTermination.R')
-source('R/GrowingDegreeDay.R')
-source('R/CheckGroundwaterTable.R')
-source('R/PreIrrigation.R')
-source('R/Drainage.R')
-source('R/RainfallPartition.R')
-source('R/Irrigation.R')
-source('R/RootZoneWater.R')
-source('R/Infiltration.R')
-source('R/CapillaryRise.R')
-source('R/Germination.R')
-source('R/GrowthStage.R')
-source('R/RootDevelopment.R')
-source('R/CanopyCover.R')
-source('R/WaterStress.R')
-source('R/SoilEvaporation.R')
-source('R/EvapLayerWaterContent.R')
-source('R/Transpiration.R')
-source('R/AerationStress.R')
-source('R/GroundwaterInflow.R')
-source('R/HIrefCurrentDay.R')
-source('R/BiomassAccumulation.R')
-source('R/TemperatureStress.R')
-source('R/HarvestIndex.R')
-source('R/CCDevelopment.R')
-source('R/AdjustCCx.R')
-source('R/CCRequiredTime.R')
-source('R/HIadjPreAnthesis.R')
-source('R/HIadjPostAnthesis.R')
-source('R/UpdateTime.R')
-source('R/ResetInitialConditions.R')
-source('R/HIadjPollination.R')
-source('R/Initialise.R')
-source('R/SoilHydraulicProperties.R')
-source('R/UpdateCCxCDC.R')
-
+source('../AquaCropR/R/Aqua_library.R')
+source('../AquaCropR/R/ReadFileLocations.R')
+source('../AquaCropR/R/ReadWeatherInputs.R')
+source('../AquaCropR/R/ReadClockParameters.R')
+source('../AquaCropR/R/ReadFieldManagement.R')
+source('../AquaCropR/R/ReadModelParameters.R')
+source('../AquaCropR/R/ReadIrrigationManagement.R')
+source('../AquaCropR/R/ReadGroundwaterTable.R')
+source('../AquaCropR/R/ComputeVariables.R')
+source('../AquaCropR/R/ComputeCropCalendar.R')
+source('../AquaCropR/R/CalculateHILinear.R')
+source('../AquaCropR/R/CalculateHIGC.R')
+source('../AquaCropR/R/ReadModelInitialConditions.R')
+source('../AquaCropR/R/PerformSimulation.R')
+source('../AquaCropR/R/ExtractWeatherData.R')
+source('../AquaCropR/R/Solution.R')
+source('../AquaCropR/R/CheckModelTermination.R')
+source('../AquaCropR/R/GrowingDegreeDay.R')
+source('../AquaCropR/R/CheckGroundwaterTable.R')
+source('../AquaCropR/R/PreIrrigation.R')
+source('../AquaCropR/R/Drainage.R')
+source('../AquaCropR/R/RainfallPartition.R')
+source('../AquaCropR/R/Irrigation.R')
+source('../AquaCropR/R/RootZoneWater.R')
+source('../AquaCropR/R/Infiltration.R')
+source('../AquaCropR/R/CapillaryRise.R')
+source('../AquaCropR/R/Germination.R')
+source('../AquaCropR/R/GrowthStage.R')
+source('../AquaCropR/R/RootDevelopment.R')
+source('../AquaCropR/R/CanopyCover.R')
+source('../AquaCropR/R/WaterStress.R')
+source('../AquaCropR/R/SoilEvaporation.R')
+source('../AquaCropR/R/EvapLayerWaterContent.R')
+source('../AquaCropR/R/Transpiration.R')
+source('../AquaCropR/R/AerationStress.R')
+source('../AquaCropR/R/GroundwaterInflow.R')
+source('../AquaCropR/R/HIrefCurrentDay.R')
+source('../AquaCropR/R/BiomassAccumulation.R')
+source('../AquaCropR/R/TemperatureStress.R')
+source('../AquaCropR/R/HarvestIndex.R')
+source('../AquaCropR/R/CCDevelopment.R')
+source('../AquaCropR/R/AdjustCCx.R')
+source('../AquaCropR/R/CCRequiredTime.R')
+source('../AquaCropR/R/HIadjPreAnthesis.R')
+source('../AquaCropR/R/HIadjPostAnthesis.R')
+source('../AquaCropR/R/UpdateTime.R')
+source('../AquaCropR/R/ResetInitialConditions.R')
+source('../AquaCropR/R/HIadjPollination.R')
+source('../AquaCropR/R/Initialise.R')
+source('../AquaCropR/R/SoilHydraulicProperties.R')
+source('../AquaCropR/R/UpdateCCxCDC.R')
 
 plot_scatter <- function(u, t, folder_name){
   
@@ -85,14 +84,11 @@ plot_scatter <- function(u, t, folder_name){
 #library('AquaCropR')
 #
 #
-folder_names <- dir(pattern='input_maize*')
-
-tiff('test.tiff', res=100)
-par(mfrow = c(2,2),mar=c(0.95,2,0.9,0.4), oma=c(1.5,2,1,1))
-for(folder_name in folder_names){
+    folder_name <- dir(pattern='input_maize*')
+ 
     FileLocation = ReadFileLocations(paste(folder_name,'/', 'filesetup.xml', 
                                            sep=''))
-    break
+    #break
     InitialiseStruct <- Initialise(FileLocation)
     
     Outputs <- PerformSimulation(InitialiseStruct)
@@ -104,23 +100,24 @@ for(folder_name in folder_names){
     i = lapply(Outputs_month, function(x) x[as.numeric(which(x$Yield == 
                                                                max(x$Yield)))][1])
     u = data.frame(t(data.frame(rbind(sapply(i, function(x) x)))))
-    
+    #break
     d <- list()
     d[['RefBio']] <- 'Biomass (g m-2)'
-    d[['Yield']] <- 'Yield tonne/ha'
+    d[['Yield']] <- 'Yield t/ha'
     d[['CC']] <- 'Canopy cover (%)'
     d[['Infl']] <- 'Infiltration (mm)'
     d[['Irr']] <- 'Irrigation (mm)'
     d[['Et0']] <- 'Et0'
     
-  
+   
+ 
     for(cname in names(d)){
-      tiff(paste(FileLocation$Output, 'Figure_', cname, '.tiff', sep=''),  width = 800,
-           height = 600, res = 145)
-      p <- ggplot(Outputs, aes(x = TotGDD, y = Outputs[[cname]], col = PlantingDate)) +
-        geom_density(alpha = 0.5)
-      + 
-        theme_bw() +  labs(y = d[[cname]], x = 'total Degree Day') +
+      tiff(paste(FileLocation$Output, 'Figure_', cname, '.tiff', sep=''),  
+            width = 800,height = 600, res = 145)
+      p <- ggplot(Outputs, aes(x = TotGDD, y = Outputs[[cname]], 
+                               col = PlantingDate)) +
+        geom_line(aes(linetype=PlantingDate, color=PlantingDate), size = 0.7) + 
+        theme_bw() +  labs(y = d[[cname]], x = 'cum Degree Day (cd)') +
         theme(axis.title.x = element_text(size = 16),
               axis.title.y = element_text(size = 16),
               axis.text.x = element_text(size = 11.5, angle = 90),
@@ -128,15 +125,9 @@ for(folder_name in folder_names){
               # legend.text = element_text(size = 12),
               #legend.position="bottom")
               legend.position = "none")
-      #legend.key.size = unit(2,"line")) 
-      #+  facet_grid(PlantingDate ~ .)
-      print(p) 
-      
-      Laura1974
-      dev.off()  
-    }
-    
-    
+      print(p)
+      dev.off() 
    
-}
-dev.off() 
+    }
+
+
